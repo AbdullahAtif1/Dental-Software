4f6bec4a1bee3e7060874debf06712e704a600ce
AbdullahAtif1 <91042316+AbdullahAtif1@users.noreply.github.com>
2024-08-06 23:34:33 +0500

    appointment view written

--- main/views.py
+++ main/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
-
+from .forms import AppointmentForm
+from django.http import JsonResponse
 
 def index(request):
-	return render(request, 'main/index.html')
+
+	form = AppointmentForm()
+	if request.method == "POST":
+		form = AppointmentForm(request.POST)
+		if form.is_valid():
+			instance = form.save(commit=False)
+			instance.patient = request.user
+			instance.save()
+			
+			if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+				return JsonResponse({'message': "Your appointment is under review. We'll notify you soon in case of an update. Thank You"})
+		else:
+			if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+				return JsonResponse({'message': 'There was an error with your submission. Please try again later'}, status=400)
+
+	else:
+		form = AppointmentForm()
+
+	context = {'form': form}
+
+	return render(request, 'main/index.html', context)
 
